8e3e516ebfae241d243d7bc351cf9faaac8955e5
Giovanni <giovanpi@stud.ntnu.no>
2020-09-20 17:05:00 +0200

    aggiunti trattamenti

--- page-home.php
+++ page-home.php
@@ -17,15 +17,10 @@ get_header();
 
 	<main id="primary" class="site-main">
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
-
-			get_template_part( 'template-parts/content', 'page' );
-
-
-		endwhile; // End of the loop.
-		?>
+		<div class="grid-container">
+			<div class="grid-x">
+			</div>
+		</div>
 
 	</main><!-- #main -->
 
