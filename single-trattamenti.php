7e9e52390450c8587fd70bdf3133e9b156eac80e
Giovanni <giovanpi@stud.ntnu.no>
2020-09-21 21:45:08 +0200

    rimossi estra da trattamenti

--- single-trattamenti.php
+++ single-trattamenti.php
@@ -26,92 +26,21 @@ get_header();
 				</div>
 
 				<div class="cell titoli">
+					<h3 class="titolo corsivo"> Trattamenti</h3>
+					<h2 class="sottotitolo maiuscolo"> <?php 
+					$category_detail=get_the_category();
 
-					<?php the_title( '<h3 class="titolo corsivo">', '</h3>' ); ?>
-
-					<h2 class="sottotitolo maiuscolo"> <?php the_field('sottotitolo'); ?></h2>
-
-				</div>
-
-
-				<div class="cell timerContainer small-12 medium-12 large-12">
-					
-					<div class="grid-x">
-
-				
-				<?php 
-					$data_di_scadenza = get_field('data_di_scadenza');
-
-					// Create DateTime object from value (formats must match).
-
-					$scadenza = new DateTime($data_di_scadenza, new DateTimeZone('Europe/Rome'));
-
-					$adesso = new DateTime( "",new DateTimeZone('Europe/Rome'));
-					
-					$intervallo = $scadenza->diff($adesso);
-					$intervallos= $scadenza - $adesso;
-
-					$giorni =  $intervallo ->days;
-
-				?>
+					foreach($category_detail as $cd){
+						echo $cd->cat_name;
+					}
+					echo " ";
+					echo get_the_title(); ?></h2>
 
 
-				<?php if ($giorni > 0): ?>
-					<div class="cell timer small-12 medium-4 large-4 first_countdown">
-
-						<div id="countdownOfferta_giorni" data-date="<?php echo get_field('data_di_scadenza') ;?>" class="countdownBox " style=""></div>
-
-					</div>
-					<div class="cell timer small-6 medium-4 large-4">
-
-		
-
-				<?php else: ?>
-					<div class="cell timer small-12 medium-4 large-4 first_countdown">
-				<?php endif; ?>
-
-						<div id="countdownOfferta_ore" data-date="<?php echo get_field('data_di_scadenza') ;?>" class="countdownBox" style=""></div>
-
-					</div>
-
-				<div class="cell timer small-6 medium-4 large-4">
-
-					<div id="countdownOfferta_minuti" data-date="<?php echo get_field('data_di_scadenza') ;?>" class="countdownBox" style=""></div>
-				</div>
-
-
-					
-
-
-
-					<?php if ($giorni < 1): ?>
-						<div class="cell timer cell small-6 medium-3 large-3">
-
-							<div id="countdownOfferta_secondi" data-date="<?php echo get_field('data_di_scadenza') ;?>" class="countdownBox" style=""></div>
-							</div>
-
-					<?php endif; ?>
-					
-					</div>
-					</div>
-
-					<div class="cell" id="offerta_scaduta" style="<?php if ($adesso  < $scadenza) { echo "display: none;"; }?>">
-
-						<h2 class="offerta_scaduta">Questa offerta è scaduta.</h2>
-					</div>
-
-
-				<div class="cell prezzi">
-
-					<h4 class="prezzo barrato"> <span class="strikethrough"><?php the_field('prezzo_intero'); ?>€</span></h4>
-					<h2 class="prezzo"> <?php the_field('prezzo_ridotto'); ?>€</h2>
-
-					<p class="commento_prezzo"> <?php the_field('commento_prezzo'); ?> </p>
+					 
 
 				</div>
-			</div>
 
-			
 
 
 				<?php
@@ -168,26 +97,6 @@ get_header();
 
 				?>
 
-
-
-
-
-
-
-
-		<div class="grid-x">
-			<div class="cell small-12 medium-8 large-6 large-offset-3 medium-offset-2 contact-container">
-				<h4 class="titolo corsivo"> Contattaci</h4>
-				<h5 class="sottotitolo maiuscolo">PRENOTA L’OFFERTA</h5>
-				<p>Compilate il form e Sarete contattati telefonicamente dal nostro staff. Insieme fisseremo la data per il trattamento.</p>
-			</div>
-
-			<div class="cell small-10 medium-4 large-4 small-offset-1 large-offset-4 medium-offset-4 form">
-
-			<?php echo do_shortcode('[contact-form-7 id="46" title="Offerte a tempo"]'); ?>
-			
-			</div>
-		</div>
 
 
 
