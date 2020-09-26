98d1c52a04d7488e31755506cadb1a6e7010fa7a
Giovanni <giovanpi@stud.ntnu.no>
2020-09-26 23:47:04 +0200

    fix scroll to ancor

--- js/trattamenti.js
+++ js/trattamenti.js
@@ -45,9 +45,31 @@ jQuery(document).ready(function () {
 
 
     $( "ul.sub-menu-element li.menu-element a" ).click(function() {
-        setTimeout(function(){ menu_scroll();}, 1);
-      
-    });
+
+        
+
+  // Remove the # from the hash, as different browsers may or may not include it.hash
+        var link = $(this).attr('href');
+        var hash = "";
+        if (link.indexOf("#") != -1) {
+        // Your code in here accessing the string like this
+        hash = link.substr(location.href.indexOf("#")).replace('#','');
+        }
+         console.log(hash);
+
+
+
+         if(hash != ''){
+
+            
+            // Clear the hash in the URL
+            // location.hash = '';   // delete front "//" if you want to change the address bar
+             $('html, body').animate({ scrollTop: $('#element'+hash).offset().top}, 1000);
+
+            }
+             
+           
+         });
 
 
 })
