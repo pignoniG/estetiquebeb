3daadb49c435816dc56eefb8c32fa985d11eb17a
Giovanni <giovanpi@stud.ntnu.no>
2020-10-04 11:39:15 +0200

    404

--- 404.php
+++ 404.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * The template for displaying 404 pages (not found)
+ * The template for displaying all single posts
  *
- * @link https://codex.wordpress.org/Creating_an_Error_404_Page
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
  *
  * @package estetiquebeb
  */
@@ -12,49 +12,27 @@ get_header();
 
 	<main id="primary" class="site-main">
 
-		<section class="error-404 not-found">
-			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'estetiquebeb' ); ?></h1>
-			</header><!-- .page-header -->
+		<div class="grid-container">
+			<div class="grid-x">
 
-			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'estetiquebeb' ); ?></p>
 
-					<?php
-					get_search_form();
+				<div class="cell titoli">
 
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
+					<h3 class="titolo corsivo">Questa pagina non esiste più o hai selezionato un indirizzo errato</h3>
 
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'estetiquebeb' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
+					<h2 class="sottotitolo maiuscolo">PAGINA NON TROVATA</h2>
 
-					<?php
-					/* translators: %1$s: smiley */
-					$estetiquebeb_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'estetiquebeb' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$estetiquebeb_archive_content" );
 
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
+				</div>
 
-			</div><!-- .page-content -->
-		</section><!-- .error-404 -->
+			</div>
+			
+
+		</div>
+
 
 	</main><!-- #main -->
 
 <?php
+
 get_footer();
