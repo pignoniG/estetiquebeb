55b89effec8e628c7211f3f57df0712d464aeb48
Giovanni <giovanpi@stud.ntnu.no>
2020-10-12 14:59:01 +0200

    p.policy 2

---
+++ page-privacy-policy.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package estetiquebeb
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+
+		<div class="grid-container">
+			<div class="grid-x">
+
+	
+				<div class="cell titoli">
+
+					<a href="https://www.iubenda.com/privacy-policy/92045316" class="iubenda-white no-brand iubenda-embed iub-body-embed" title="Privacy Policy">Privacy Policy</a><script type="text/javascript">(function (w,d) {var loader = function () {var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src="https://cdn.iubenda.com/iubenda.js"; tag.parentNode.insertBefore(s,tag);}; if(w.addEventListener){w.addEventListener("load", loader, false);}else if(w.attachEvent){w.attachEvent("onload", loader);}else{w.onload = loader;}})(window, document);</script>
+
+					<a href="https://www.iubenda.com/privacy-policy/92045316/cookie-policy" class="iubenda-white no-brand iubenda-embed iub-body-embed" title="Cookie Policy">Cookie Policy</a><script type="text/javascript">(function (w,d) {var loader = function () {var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src="https://cdn.iubenda.com/iubenda.js"; tag.parentNode.insertBefore(s,tag);}; if(w.addEventListener){w.addEventListener("load", loader, false);}else if(w.attachEvent){w.attachEvent("onload", loader);}else{w.onload = loader;}})(window, document);</script>
+
+					<a href="https://www.iubenda.com/termini-e-condizioni/92045316" class="iubenda-white no-brand iubenda-embed iub-body-embed" title="Termini e Condizioni">Termini e Condizioni</a><script type="text/javascript">(function (w,d) {var loader = function () {var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src="https://cdn.iubenda.com/iubenda.js"; tag.parentNode.insertBefore(s,tag);}; if(w.addEventListener){w.addEventListener("load", loader, false);}else if(w.attachEvent){w.attachEvent("onload", loader);}else{w.onload = loader;}})(window, document);</script>
+
+
+				</div>
+
+			</div>
+		</div>
+			
+
+
+
+	</main><!-- #main -->
+
+<?php
+
+get_footer();
