ab7300ed7e94e5e57fb900558d29bd1d663c4834
Giovanni <giovanpi@stud.ntnu.no>
2020-10-13 21:45:36 +0200

    transizione

--- js/aftermath.js
+++ js/aftermath.js
@@ -3,12 +3,13 @@ $( document ).ready(
 
 
     	if (!$( "body" ).hasClass( "home" )) {
-    		$(".shameful_cover").fadeOut( "400");
+    		$(".shameful_cover").fadeOut( "100");
             setTimeout(function(){menu_scroll()}, 200);
     	}
     	else{
+            $(".shameful_cover svg").css("top", "0");
 
-            setTimeout(function(){$(".shameful_cover").fadeOut( "400");}, 400);
+            setTimeout(function(){$(".shameful_cover").fadeOut( "400");}, 2200);
         }
 
     	
