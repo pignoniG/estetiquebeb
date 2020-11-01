d6d57bfb599089386fcdaba47505d7934e177e88
Giovanni <giovanpi@stud.ntnu.no>
2020-11-01 16:41:07 +0100

    note rimosse

--- functions.php
+++ functions.php
@@ -321,7 +321,40 @@ function woo_custom_product_add_to_cart_text() {
   
 }
 
+// Remove the additional information tab
+function woo_remove_product_tabs( $tabs ) {
+    unset( $tabs['additional_information'] );
+    return $tabs;
+}
+add_filter( 'woocommerce_product_tabs', 'woo_remove_product_tabs', 98 );
 
+add_filter('woocommerce_enable_order_notes_field', '__return_false');
+
+// remove Order Notes from checkout field in Woocommerce
+add_filter( 'woocommerce_checkout_fields' , 'alter_woocommerce_checkout_fields' );
+function alter_woocommerce_checkout_fields( $fields ) {
+
+     unset($fields['billing']['billing_company']); // remove the option to enter in a company
+     unset($fields['billing']['billing_address_2']); // remove the second line of the address
+    
+     unset($fields['billing']['billing_country']); // remove the billing country
+     unset($fields['billing']['billing_postcode']); // remove the billing state
+
+     unset($fields['shipping']['shipping_first_name']);
+     unset($fields['shipping']['shipping_last_name']);
+     unset($fields['shipping']['shipping_company']);
+     unset($fields['shipping']['shipping_address_1']);
+     unset($fields['shipping']['shipping_address_2']);
+     unset($fields['shipping']['shipping_city']);
+     unset($fields['shipping']['shipping_postcode']);
+     unset($fields['shipping']['shipping_country']);
+     unset($fields['shipping']['shipping_state']);
+     unset($fields['account']['account_username']); // removing this or the two fields below would prevent users from creating an account, which you can do via normal WordPress + Woocommerce capabilities anyway
+     unset($fields['account']['account_password']);
+     unset($fields['account']['account_password-2']);
+     unset($fields['order']['order_comments']); // removes the order comments / notes field
+     return $fields;
+}
 /**
  * Implement the Custom Header feature.
  */
